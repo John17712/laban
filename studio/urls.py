d4d7853b3d6aa4d30baf2e33f5199c23ccaeb10f
John <2003l.jm@gmail.com>
2023-09-01 13:01:11 +0300

    first commit

---
+++ studio/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index, name='index'),
+    path('index.html',views.index, name='index'),
+    path('about.html',views.about, name='about'),
+    
+    path('contact.html',views.contact, name='contact'),
+    path('gallery.html',views.gallery, name='gallerly'),
+    path('main.html',views.main, name='main'),
+    path('pricing.html',views.pricing, name='pricing'),
+    path('services.html',views.services, name='services'),    
+    path('appointment.html',views.appointment, name='appointment'),
+    
+    
+
+]
